68444fbf2257c985691aec622323bd15c74add91
Anurag srivastava <anuragsrivastava8874@gmail.com>
2026-01-27 12:52:08 +0530

    done frontern set up

---
+++ frontend/src/App.jsx
@@ -0,0 +1,16 @@
+
+import React from 'react'
+import Navbar from './components/Navbar';
+
+const App = () => {
+  return (
+   
+    <div className="h-screen bg-black flex items-center justify-center">
+      <h1 className="text-white text-5xl font-bold">
+        Frontend Ready 🚀
+      </h1>
+    </div>
+  )
+}
+
+export default App;
