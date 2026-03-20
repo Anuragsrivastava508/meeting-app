2871bd8f40bacdd464a247bd90ab91c2445029a5
Anurag srivastava <anuragsrivastava8874@gmail.com>
2026-03-20 14:44:24 +0530

    DONE

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,9 @@
 
 import React from 'react'
 import Navbar from './components/Navbar';
-import { Routes,Route } from 'react-router-dom';
+import { Routes,Route,Navigate } from 'react-router-dom';
+import { Loader } from "lucide-react";
+import { Toaster } from "react-hot-toast";
 
 import HomePages from './pages/homePages';
 import SignupPages from './pages/SignupPages';
@@ -13,26 +15,50 @@ import { useAuthStore } from "./store/useAuthStore";
 import { useEffect } from 'react';
 
 const App = () => {
-const {authUser,checkAuth} = useAuthStore();
+const {authUser,checkAuth,isCheckingAuth} = useAuthStore();
 
 useEffect(()=>{
   checkAuth();
 },[checkAuth] );
 
 console.log({authUser});
+if (isCheckingAuth && !authUser) {
+    return (
+      <div className="flex items-center justify-center h-screen">
+        <Loader className="size-10 animate-spin" />
+      </div>
+    );
+  }
 
   return (
   <div>
     <Navbar/>
     <Routes>
-    <Route path='/' element={<HomePages/>}/>
-    <Route path='/Signup' element={<SignupPages/>}/>
-    <Route path='/Login' element={<LoginPages/>}/>
-    <Route path='/Settings' element={<SettingPages/>}/>
-    <Route path='/Profile' element={<ProfilePages/>}/>
+       <Route
+          path="/"
+          element={authUser ? <HomePages /> : <Navigate to="/Login" />}
+        />
+   
+        <Route
+          path="/"
+          element={authUser ? <HomePages /> : <Navigate to="/Login" />}
+        />
+        <Route
+          path="/signup"
+          element={!authUser ? <SignupPages /> : <Navigate to="/" />}
+        />
+        <Route
+          path="/login"
+          element={!authUser ? <LoginPages /> : <Navigate to="/" />}
+        />
+        <Route path="/settings" element={<SettingPages />} />
+        <Route
+          path="/profile"
+          element={authUser ? <ProfilePages /> : <Navigate to="/Login" />}
+        />
+      </Routes>
 
-    </Routes>
-    
+      <Toaster />
   </div>
   )
 }
