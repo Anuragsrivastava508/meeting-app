8b75e6592f5e5c32f5f43bb276c09df85da1d10f
Anurag srivastava <anuragsrivastava8874@gmail.com>
2026-04-04 00:09:29 +0530

    fixinf=g the file

--- backend/src/server.js
+++ backend/src/server.js
@@ -20,14 +20,18 @@ dotenv.config();
 const app = express();
 
 /* ================= MIDDLEWARE ================= */
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+ app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ limit: "10mb", extended: true }));
 app.use(cookieParser());
+
 
 app.use(cors({
   origin: "http://localhost:5173",
   credentials: true,
 }));
+
+
+
 
 /* ================= ROUTES ================= */
 app.get("/", (req, res) => {
